1814fad49735a321093d23f7696ade1715851d49
Mariano <79314263+Azzeri@users.noreply.github.com>
2022-11-26 20:40:58 +0100

    plerdy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,5 +24,17 @@
         @env ('local')
             <script src="http://localhost:8080/js/bundle.js"></script>
         @endenv
+         <!-- BEGIN PLERDY CODE -->
+        <script type="text/javascript" defer data-plerdy_code='1'>
+            var _protocol="https:"==document.location.protocol?" https://":" http://";
+            _site_hash_code = "a99594ff4d81f21031e4049a89f4d2d5",_suid=32469, plerdyScript=document.createElement("script");
+            plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+            plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+            var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+            plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+            try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
+        </script>
+        <!-- END PLERDY CODE -->
+
     </body>
 </html>
