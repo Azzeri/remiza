a87c52ce769228097b62f2bccfed818d9712524a
Mariano <79314263+Azzeri@users.noreply.github.com>
2022-11-26 21:24:15 +0100

    analysis

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,13 +17,18 @@
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
         <script src="https://kit.fontawesome.com/093e387b29.js" crossorigin="anonymous"></script>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WCN6VW4');</script>
-        <!-- End Google Tag Manager -->
+       
+       <!-- Hotjar Tracking Code for https://remiza.herokuapp.com/ -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3265092,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
@@ -31,11 +36,6 @@
         @env ('local')
             <script src="http://localhost:8080/js/bundle.js"></script>
         @endenv
-
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCN6VW4"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
 
     </body>
 </html>
